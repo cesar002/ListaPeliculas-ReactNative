ade9de73cc465e590902ca1d1063e1041a8d1cd5
Cesar002 <soul.unleashed13@gmail.com>
2018-03-14 16:13:16 -0600

    Se agregó la funcionalidad de registrar una pelicula utilizando axios

--- Screens/AddPeli.js
+++ Screens/AddPeli.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import {View, StyleSheet, Text, TextInput, Button} from 'react-native';
+import {View, StyleSheet, Text, TextInput, Button, Alert} from 'react-native';
+import axios from 'axios';
 
 
 const styles = StyleSheet.create({
@@ -18,27 +19,42 @@ const styles = StyleSheet.create({
 
 
 class Form extends Component{
+    constructor(props){
+        super(props);
+
+        this.setPeli = this.setPeli.bind(this);
+
+        this.state = {
+            titulo: '',
+            fecha: '',
+            director: ''
+        };
+    }
+
     render(){
         return(
             <View>
                 <View style={{padding: 10}}>
                     <TextInput
                         style={{height: 40}}
-                        placeholder="Titulo de la pelicula"
+                        placeholder = "Titulo de la pelicula"
+                        onChangeText = { (titulo) => this.setState({titulo}) }
                     />
                     <TextInput
                         style={{height: 40}}
                         placeholder="Año"
+                        onChangeText = {(fecha) => this.setState({fecha})} 
                     />
                     <TextInput
                         style={{height: 40}}
                         placeholder="Director"
+                        onChangeText = {(director) => this.setState({director})}
                     />
                 </View>
                 <View style={styles.ContenedorBoton}>
                     <Button
                         onPress={
-                            ()=>{}
+                            () => this.setPeli(this.state)
                         }
                         title="Agregar"
                         color="#FA8072"
@@ -46,6 +62,24 @@ class Form extends Component{
                 </View>
             </View>
         );
+    }
+
+    setPeli(state){
+        let formData = new FormData();
+
+        formData.append('Titulo',state.titulo);
+        formData.append('Fecha',state.fecha);
+        formData.append('Director', state.director);
+
+        axios({
+            method: 'post',
+            url: 'http://192.168.1.70:8080/pelicula/create',
+            data: formData,
+        }).then(response => {
+            Alert.alert('Pelicula insertada');
+        }).catch(error => {
+            Alert.alert('Error al conectar');
+        })
     }
 }
 
